709787b170ebc70106fc6fb8f6583e0f9332727e
dumuzhou <dengyin90@163.com>
2016-08-05 02:30:01 +0000

    auto

--- README.md
+++
@@ -1,3 +0,0 @@
-
-
-[blog](http://dumuzhou.github.io/)
--- push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-svn status | grep "!" | awk '{print $2}' | while read line;do svn delete ${line};done;
-svn status | grep "?" | awk '{print $2}' | while read line;do svn add ${line};done;
-svn commit -m 'auto'
